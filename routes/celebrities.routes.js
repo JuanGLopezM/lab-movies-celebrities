f85bc490f7ecb286ae7061a2cb972b3496c22e04
JuanGLopezM <105565395+JuanGLopezM@users.noreply.github.com>
2022-06-28 15:15:41 +0200

    prueba

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,19 @@
 const express = require("express");
-const prueba = require("../models/Drone.model");
+const Celebrity = require("../models/celebrity.model");
 const router = express.Router();
+//requestAnimationFrame("../db");
 
+router.get("/celebrities/create", (req, res, next) =>
+  res.render("celebrities/new-celebrity.hbs")
+);
+
+router.post("/celebrities/create", (req, res, next) => {
+  console.log(req.body);
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.create({ name, occupation, catchPhrase })
+
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => res.render("celebrities/new-celebrity.hbs"));
+});
 module.exports = router;
